3b83b7c9cc55ecc1a062c701c953794300d1f007
Elena Wang <ewang88@smith.edu>
2025-07-09 06:47:30 -0500

    testing code + adding documentation from CSC 356

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,7 @@
 class MainController < ApplicationController
 
   def index
+    @menu = CbordMenu.where("service_unit = 'GILLETT' AND eventdate = '2024-11-19'").order("eventdate").first.formal_name
   end
   
 end
